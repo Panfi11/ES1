e7e6a3e50d7799785a4a657b715d9a7304a9b96b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-24 16:08:27 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApps2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrarys1;
+
+namespace ConsoleApps2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dammi un numero");
+            int n = int.Parse(Console.ReadLine());
+            bool risposta = Calcoli.QuadratoPerfetto(n);
+            if (risposta)
+            {
+                Console.WriteLine("il numero è un quadrato perfetto ");
+
+            }
+            else
+                Console.WriteLine("il numero non è un quadrato perfetto");
+            Console.ReadLine();
+        }
+        
+    }
+}
